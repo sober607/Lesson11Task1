b6f5c469489e3ea410f0d4018c44c9a951abcf87
sober607 <sober607@gmail.com>
2020-03-24 18:40:04 +0200

    Lesson11 task 1 first commit

---
+++ Lesson11Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lesson11Task1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запустился главный поток");
+            ParameterizedThreadStart writeSecond = new ParameterizedThreadStart(RecursThreads);
+            Thread recursthread = new Thread(writeSecond);
+            recursthread.Start(10);
+
+            Console.WriteLine("Закончился главный поток");
+            Console.ReadLine();
+        }
+        
+        public static void RecursThreads(object argument)
+        {
+             
+            for (int d= (int)argument - 1; d>0; d--)
+            {
+                Console.WriteLine("Начался поток " + d);
+                
+                    RecursThreads(d);
+                
+
+
+            }
+
+        }
+    }
+}
